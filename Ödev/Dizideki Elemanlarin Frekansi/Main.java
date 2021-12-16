7800f8915bf95a1e6d76344e14ccf5d320205a40
Elif <elifnur.telase@gmail.com>
2021-12-16 12:44:37 +0200

    Duzenlendi

--- Ödev/Dizideki Elemanlarin Frekansi/Main.java
+++ Ödev/Dizideki Elemanlarin Frekansi/Main.java
@@ -4,7 +4,6 @@ public class Main {
    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int counter = 0;
         
         System.out.print("Dizi uzunluğunu girin:");
         int[] list = new int[sc.nextInt()];
